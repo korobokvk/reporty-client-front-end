70c32bf5f24709af7f20facea4a42d9d77536076
korobokvk <korobokvk@gmail.com>
2020-03-30 18:12:40 +0300

    refactoring

--- src/config.js
+++ src/config.js
@@ -5,3 +5,5 @@ export const AUTH_URL = `${ROOT_URL}/auth`;
 export const SIGN_IN_URL = `${AUTH_URL}/sign-in`;
 export const SIGN_UP_URL = `${AUTH_URL}/sign-up`;
 export const CHECK_TOKEN_IF_VALID = `${AUTH_URL}/check-auth`;
+
+console.log(AUTH_URL);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serviceUrl: "api"
+  serviceUrl: ":3000"
 };
