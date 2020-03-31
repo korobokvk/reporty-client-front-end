9694bf405babbdc1134c6d0b426442558fd767d4
korobokvk <korobokvk@gmail.com>
2020-03-31 16:00:10 +0300

    refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
+    path: "",
+    redirectTo: "auth"
+  },
+  {
     path: "auth",
     loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule)
   },
