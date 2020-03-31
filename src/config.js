bf4f2e3fcd272f11b266890e897c03419388efaa
korobokvk <korobokvk@gmail.com>
2020-03-31 15:38:13 +0300

    refactoring

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 import { environment } from "./environments/environment";
 
-export const ROOT_URL = environment.serviceUrl;
+export const ROOT_URL =
+  "http://acfd7ed1bea40451186971170c811103-1023436132.us-east-2.elb.amazonaws.com:3000";
+//environment.serviceUrl;
 export const AUTH_URL = `${ROOT_URL}/auth`;
 export const SIGN_IN_URL = `${AUTH_URL}/sign-in`;
 export const SIGN_UP_URL = `${AUTH_URL}/sign-up`;
